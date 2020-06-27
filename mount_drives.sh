e2fe072f3f83491d1209574619b03647b8a326ca
Jorge Vásquez Pérez <yorch.vasquez@gmail.com>
2020-06-27 13:35:20 -0700

    Wraped dir in quotes to allow for spaces in dir name.

--- mount_drives.sh
+++ mount_drives.sh
@@ -1,9 +1,10 @@
 mount=/media
+export IFS=$'\n'
 
 drives=$(ssh jorge@paraulata.casa.vm ls /media )
 
 for dir in ${drives[@]}; do
-	mkdir $mount/$dir;
-	chown jorge $mount/$dir
-	sshfs jorge@paraulata.casa.vm:$mount/$dir $mount/$dir &
+	mkdir $mount/"$dir";
+	chown jorge $mount/"$dir"
+	sshfs jorge@paraulata.casa.vm:$mount/"$dir" $mount/"$dir" &
 done
